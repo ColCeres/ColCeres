a8ab1ed81458af44d3f4e4797575ea55c96ac92a
johang <dampig14@gmail.com>
2021-10-13 16:13:31 -0500

    se agregan cambios en el formulario <3

--- form.js
+++ form.js
@@ -1,88 +1,135 @@
-'use stric';
+"use stric";
 
-const formulario= document.getElementById("formulario_reg"); //Se usa para acceder al id de un formulario
-const inputs= document.querySelectorAll("#formulario_reg input") //Con el uso de query Selector se accede a todos los inputs y con la palabra "All" nos generá un arreglo (array).
+// espacio para el metodo principal
 
-
-const expresiones = {
-	nombrejuego: /^[a-z\s?A-Z\s?]{2,25}$/, // Letras, numeros, guion y guion_bajo
-	contrato: /^[a-z\s?A-Z\s?]{3,25}$/,  // Letras, pueden llevar acentos.
-	password: /^.{8,12}$/, // 4 a 12 digitos.
-	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-}
-
-// Dentro de la constante validar formulario se ejecuta una funcion tipo flecha que contiene in switch para verificar las expresiones regulares dado un target name.
-
-const validarFormulario = (e) => {
-	switch (e.target.name){     
-		case "nombrejuego":
-			Validarcampo(expresiones.nombrejuego, e.target.value, "nombre" );
-		break
-			
-		case "contrato":
-			Validarcampo(expresiones.contrato, e.target.value, "contrato" );
-		break
-
-		case "password":
-			Validarcampo(expresiones.password, e.target.value, "password" );
-			validarpassword();
-		break
-
-		case "password2":
-			validarpassword();
-		break
-
-		case "email":
-			Validarcampo(expresiones.email, e.target.value, "email" );
-		break
-
-		case "telefono":
-			Validarcampo(expresiones.telefono, e.target.value, "telefono" );
-		break
-
-	}
-}
-
-const Validarcampo = (expresiones, input, campo) => {
-	if(expresiones.test(input)){
-		document.getElementById(`formulario--${campo}`).classList.remove("formulario__grupo-incorrecto");
-		document.getElementById(`formulario--${campo}`).classList.add("formulario__grupo-correcto");
-		document.querySelector(`#formulario--${campo} i`).classList.remove("fa-times-circle");
-		document.querySelector(`#formulario--${campo} i`).classList.add("fa-check-circle");
-		document.querySelector(`#formulario--${campo} .formulario__grupo-input-error`).classList.remove("formulario__grupo-input-error-activo");
-	}else{
-		document.getElementById(`formulario--${campo}`).classList.add("formulario__grupo-incorrecto");
-		document.getElementById(`formulario--${campo}`).classList.remove("formulario__grupo-correcto");
-		document.querySelector(`#formulario--${campo} i`).classList.add("fa-times-circle");
-		document.querySelector(`#formulario--${campo} i`).classList.remove("fa-check-circle");
-		document.querySelector(`#formulario--${campo} .formulario__grupo-input-error`).classList.add("formulario__grupo-input-error-activo");
-	}
-}
-
-const validarpassword = () => {
-	const inputpassword1 = document.getElementById("password_1");
-	const inputpassword2 = document.getElementById("password_2");
-	if (inputpassword1.value !== inputpassword2.value){
-		document.getElementById(`formulario--password2`).classList.add("formulario__grupo-incorrecto");
-		document.getElementById(`formulario--password2`).classList.remove("formulario__grupo-correcto");
-		document.querySelector(`#formulario--password2 i`).classList.add("fa-times-circle");
-		document.querySelector(`#formulario--password2 i`).classList.remove("fa-check-circle");
-		document.querySelector(`#formulario--password2 .formulario__grupo-input-error`).classList.add("formulario__grupo-input-error-activo");
-	} else{
-		document.getElementById(`formulario--password2`).classList.remove("formulario__grupo-incorrecto");
-		document.getElementById(`formulario--password2`).classList.add("formulario__grupo-correcto");
-		document.querySelector(`#formulario--password2 i`).classList.remove("fa-times-circle");
-		document.querySelector(`#formulario--password2 i`).classList.add("fa-check-circle");
-		document.querySelector(`#formulario--password2 .formulario__grupo-input-error`).classList.remove("formulario__grupo-input-error-activo");
-	}
-}
-
-inputs.forEach((input)=>{
-	input.addEventListener("keyup", validarFormulario);
-	input.addEventListener("blur", validarFormulario); //El evento blur es disparado cuando un elemento ha perdido su foco
+document.addEventListener("DOMContentLoaded", function() { // funcion que escucha cuando se carga la pagina
+    document.getElementById("form-registro").addEventListener("submit", validateForm); //funcion que escucha cuando se activa el evento submit en el form
 });
 
-formulario.addEventListener("submit", (e) => {
-	e.preventDefault();
-});
+function validateForm(event) { //funcion que llama a las funciones que van a validar los campos
+
+    event.preventDefault(); // funcion que suspende el evento submit para que no se ejecute y asi validar los campos
+
+    document.getElementById('nomSpan').innerHTML = ""; // limpia el span de la alerta
+    document.getElementById('telSpan').innerHTML = ""; // limpia el span de la alerta
+    document.getElementById('telSpan').innerHTML = ""; // limpia el span de la alerta
+    document.getElementById('dirSpan').innerHTML = ""; // limpia el span de la alerta
+    document.getElementById('mailSpan').innerHTML = ""; // limpia el span de la alerta
+    document.getElementById('passSpan').innerHTML = ""; // limpia el span de la alerta
+
+    // en este espacio se pondran las variables que van a recibir el true o false de las funciones que evaluan los campos del formulario
+
+    var contrasena = checkContrasena(document.getElementById("contrasena").value); // variable que guarda el valor true o false de la validacion de los campos
+    var mail = checkCorreo(document.getElementById("correo").value); // variable que guarda el valor de true o false de validacion de correo
+    var dir = checkDir(document.getElementById("direccion").value); // variable que guarda el valor true o false de la validacion de los campos
+    var tel = checkTelefono(document.getElementById("telefono").value); // variable que guarda el valor true o false de la validacion de los campos
+    var nom = checkNombre(document.getElementById("nombre").value); // variable que guarda el valor true o false de la validacion de los campos
+
+    if (nom && tel && dir && mail && contrasena) { // condicional que evalua si las variables son true y asi activar el evento submit del form
+        this.submit();
+    }
+}
+
+// fin de espacio para medoto principal
+
+// espacio para validacion de campo Nombre
+function checkNombre(nom) {
+    const nomReg = /^([a-zA-Z ]){4,30}$/;
+    console.log("Test Nombre: " + nomReg.test(nom));
+    // console.log(nom.length);
+
+    if (!nomReg.test(nom)) {
+        document.getElementById('nomSpan').innerHTML = "Por favor verifique el nombre, minimo 4, maximo 30 y sin números.";
+        document.getElementById('nombre').focus();
+        console.log("[ERR Registro - Nombre]", "Por favor verifique el nombre, minimo 4, maximo 30 y sin números.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// fin espacio para validacion de campo Nombre
+
+// espacio para validacion de campo Telefono
+function checkTelefono(tel) {
+
+    const telReg = /^([0-9]){7,7}$/;
+    console.log("Test Telefono: " + telReg.test(tel));
+
+    if (!telReg.test(tel)) {
+        document.getElementById('telSpan').innerHTML = "Por favor verifique el numero ingresado, solo admite numeros y 7 digitos.";
+        document.getElementById('telefono').focus();
+        console.log("[ERR Registro - Telefono]", "Por favor verifique el numero ingresado, solo admite numeros y 7 digitos.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// fin espacio para validacion de campo Telefono
+
+// espacio para validacion de campo Direccion
+
+function checkDir(dir) {
+
+    const dirReg = /^([0-9a-zA-Z #-]){1,50}$/;
+    console.log("Test Dir: " + dirReg.test(dir));
+
+    if (!dirReg.test(dir)) {
+        document.getElementById('dirSpan').innerHTML = "Por favor verifique la dirección, debe tener máximo 50 caracteres y solo # y - como especiales.";
+        document.getElementById('direccion').focus();
+        console.log("[ERR Registro - Direccion]", "Por favor verifique la dirección, debe tener máximo 50 caracteres y solo # y - como especiales.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// fin espacio para validacion de campo Direccion
+
+// espacio para validacion de campo Correo
+function checkCorreo(mail) {
+
+    const regMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{1,8})+$/;
+    console.log("Test Mail: " + regMail.test(mail));
+
+    if (!regMail.test(mail)) {
+        document.getElementById('mailSpan').innerHTML = "Por favor verifique el correo; Debe tener @ y el dominio despues del @.";
+        document.getElementById('correo').focus();
+        console.log("[ERR Registro - Mail]", "Por favor verifique el correo, debe tener @ y el dominio despues del @.");
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+// fin espacio para validacion de campo Correo
+
+// espacio para validacion de campo Contraseña
+
+function checkContrasena(pass) { //funcion de validacion de contrasena
+
+    const regPass = /^(?=(?:.*\d){1})(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})\S{8,}$/; // expresion regular con el patron valido de contrasena
+    console.log("Test Pass: " + regPass.test(pass)); //Imprime en consolo si el test es true o false
+
+    if (!regPass.test(pass)) { //valida si el teste es true o false,
+        document.getElementById("passSpan").innerHTML = "La contraseña debe tener minimo 8 digitos, una mayúscula, una minúscula y un número."; //Imprime en el html en la etiqueta span con id passSpan el texto
+        document.getElementById("contrasena").focus(); // Hace el focus en el campo contrasena
+        console.log("[ERR Registro - Password]", "La contraseña debe tener minimo 8 digitos, una mayuscula, una minuscula y un numero"); //Imprime el error en consola
+        return false; //retorna false cuando no pasa el test de la expresion regular
+
+    } else {
+        return true; //retorna true si pasa el test de la expresion regular
+    }
+}
+
+// fin espacio para validacion de campo Contraseña
+
+// module.exports = {
+//     checkNombre,
+//     checkTelefono,
+//     checkDir,
+//     checkCorreo,
+//     checkContrasena
+// }
